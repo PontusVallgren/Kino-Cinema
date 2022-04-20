954dfcdc6eb83414b3d47e667a8902dd480f8be8
kaulfield23 <kaulfield23@gmail.com>
2022-04-20 23:39:05 +0200

    added custom Btn

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,50 +1,13 @@
-import { Box, FormControlLabel, FormGroup, TextField } from "@mui/material";
-import { NextPage } from "next";
-import React, { useState } from "react";
-import {
-  CenterHorizon,
-  CustomBtn,
-  CustomButton,
-  CustomText,
-} from "../components/CustomMUI/CustomUI";
-import classes from "../../styles/login.module.css";
+import { Box } from "@mui/material";
+import React from "react";
 
-const Login: NextPage = () => {
-  const [isMember, setIsNewMember] = useState<boolean>(true);
-
+const login = () => {
   return (
-    <div className="main">
-      {isMember ? (
-        <>
-          <CustomText className={classes.loginTitle}>
-            Välkommen till Risbäck
-          </CustomText>
-          <CenterHorizon component="form">
-            <FormGroup aria-label="position">
-              <TextField
-                id="outlined-basic"
-                label="Username"
-                className={classes.userInput}
-                color="info"
-              />
-              <TextField
-                id="outlined-basic"
-                label="Username"
-                className={classes.userInput}
-              />
-              <CustomBtn
-                btnText={"Log in"}
-                btnPadding={"20px"}
-                btnColor={"secondary"}
-              />
-            </FormGroup>
-          </CenterHorizon>
-        </>
-      ) : (
-        "no"
-      )}
+    <div>
+      login
+      <Box sx={{ height: "900px" }}>hello</Box>
     </div>
   );
 };
 
-export default Login;
+export default login;
