eb820e8d03087ac84a04f713e2009e635393b433
JohanEkstroem <90635692+JohanEkstroem@users.noreply.github.com>
2022-04-21 08:41:56 +0200

    Created a boilerplate for My Pages

---
+++ src/pages/mypages.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+export default function myPages() {
+  return (
+    <div>
+      <h1>Welcome to My Pages </h1>
+      <Box sx={{ height: '500px' }}>My Pages</Box>
+    </div>
+  );
+}
