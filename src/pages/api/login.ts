43447bed6b200dd4e172595e455e70f39f7eccd8
kaulfield23 <kaulfield23@gmail.com>
2022-04-21 20:51:44 +0200

    send cookie back

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -4,16 +4,39 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const USERS: Record<string, string> = {
   haeju: "potato",
+  johan: "what are you looking at",
 };
-const ENC_KEY = process.env.ENC_KEY;
-console.log(process.env.ENC_KEY, "EYEYE");
-
-export default async function handler(
+export default async function handleUserInfo(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method == "POST") {
-    res.json(ENC_KEY);
-    console.log(ENC_KEY, "yeyeye");
+    const { userName, userPassword } = req.body;
+
+    if (USERS[userName] && USERS[userName] == userPassword) {
+      const cookies = new Cookies(req, res);
+
+      if (!process.env.ENC_KEY) {
+        throw Error("ENC_KEY doesn't exist in .env.local");
+      }
+
+      const ENC_KEY = process.env.ENC_KEY;
+      cookies.set(
+        "session",
+        await Iron.seal(
+          {
+            username: userName,
+            loggedIn: true,
+          },
+          ENC_KEY,
+          Iron.defaults
+        )
+      );
+      res.status(200).end();
+    } else {
+      res.status(401).end();
+    }
+  } else {
+    res.status(405).end();
   }
 }
