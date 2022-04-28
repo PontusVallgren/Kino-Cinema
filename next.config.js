14a0e5dc74e3a322a69cd222c942cf5b907c0ca4
JohanEkstroem <90635692+JohanEkstroem@users.noreply.github.com>
2022-04-28 09:47:04 +0200

    Added two urls in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["i.imgur.com", "catalog.cinema-api.com"],
+    domains: [
+      'i.imgur.com',
+      'catalog.cinema-api.com',
+      'morbius.tthelostcity.com',
+      'www.saltypopcorn.co.uk',
+    ],
   },
 };
 
