827aa2536e34e13842f49064a66975fce4903b74
JohanEkstroem <90635692+JohanEkstroem@users.noreply.github.com>
2022-04-29 08:12:21 +0200

    Username is now rendered from cookie to Mina Sidor

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,5 +1,6 @@
-import { ClassNames } from '@emotion/react';
-import { NextPage } from 'next';
+import Cookies from 'cookies';
+import Iron from '@hapi/iron';
+import { GetServerSideProps, NextPage } from 'next';
 import React from 'react';
 import {
   CenterHorizon,
@@ -8,7 +9,37 @@ import {
 } from '../../components/CustomMUI/CustomUI';
 import classes from './index.module.css';
 
-const MyPage: NextPage = () => {
+//const ENC_KEY = 'Jm!S!xQ?mtyhH@a4XUU3ka?HYXx^86FA@^6sp5_6%Be9x8B^7D$Z#Fe2pc&*Q%';
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const cookies = new Cookies(context.req, context.res);
+  const sessionStr = cookies.get('session');
+  const ENC_KEY =
+    process.env.ENC_KEY || 'default_key_for_risback_cinema_hello_there';
+
+  if (sessionStr) {
+    try {
+      const session = await Iron.unseal(sessionStr, ENC_KEY, Iron.defaults);
+      if (session.loggedIn) {
+        return {
+          props: {
+            username: session.username,
+          },
+        };
+      }
+    } catch (err) {
+      //Incorrect enctrypted string
+    }
+  }
+  return {
+    notFound: true,
+  };
+};
+
+type MyPageProps = {
+  username: string;
+};
+const MyPage: NextPage<MyPageProps> = ({ username }) => {
   return (
     <div className="main">
       <CustomButton color="error" variant="contained">
@@ -30,11 +61,29 @@ const MyPage: NextPage = () => {
               </label>
               <label>
                 Användarnamn
-                <h4>JohanEkstroem</h4>
+                <h4>{username}</h4>
               </label>
             </div>
             <div className={classes.tickets}>
               <h2>Mina biljetter</h2>
+              <ul>
+                <li>
+                  <p>Filmtitel</p>
+                  <p>Tid / Visning</p>
+                </li>
+                <li>
+                  <span>The Godfather</span>
+                  <span>18.02.2022 09:00</span>
+                </li>
+                <li>
+                  <span>Idiocracy</span>
+                  <span>18.02.2022 09:00</span>
+                </li>
+                <li>
+                  <span>The Batman</span>
+                  <span>18.02.2022 09:00</span>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
