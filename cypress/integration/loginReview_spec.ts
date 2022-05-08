0d2e78cec92ce994db0fff33c12700f9cd132759
Carola Lindfors Sejsing <89970224+clindforsSejsing@users.noreply.github.com>
2022-05-08 16:13:46 +0200

    test with login and review in same test- password in gitignore cypress.env-file

--- cypress/integration/loginReview_spec.ts
+++ cypress/integration/loginReview_spec.ts
@@ -4,10 +4,14 @@ describe("usertesting 'Logga in'-page", () => {
   it("user should be able to visit login-page, login and fail to login", () => {
     cy.visit("/login");
   });
-  it("login correct, visit'mina sidor' and logout", () => {
+  it("login correct, visit'mina sidor', visit moviepage and write a review and  then logout", () => {
     const newText = "I liked this movie. It was awesome!! <3";
-    const username = Cypress.env("CYPRESS_TEST_USERNAME");
-    const password = Cypress.env("CYPRESS_TEST_PASSWORD");
+    const username = Cypress.env("username");
+    const password = Cypress.env("password");
+
+    if (typeof password !== "string" || !password) {
+      throw new Error("Missing password value");
+    }
     cy.request({
       method: "POST",
       url: "/api/login",
@@ -17,13 +21,17 @@ describe("usertesting 'Logga in'-page", () => {
       body: JSON.stringify({
         userName: `${username}`,
         userPassword: `${password}`,
-        session: true,
-        loggedIn: true,
       }),
     });
-    // get(".MuiButton-root").click();
-    cy.visit("/mypage")
-      .contains("Filmer")
+    cy.get("#Username").type(username, { log: false });
+    cy.get("#userPassword").type(password, { log: false });
+
+    cy.get(".MuiButton-root")
+      .click()
+      .get(".tss-t0jjb1-logAlarm > .MuiButton-root")
+      .click();
+
+    cy.contains("Filmer")
       .click()
       .get(
         ':nth-child(2) > .MovieItem_link__8D7kt > [style="box-sizing: border-box; display: inline-block; overflow: hidden; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; position: relative; max-width: 100%;"] > .MovieItem_thumbnail__IeN4F'
@@ -34,22 +42,11 @@ describe("usertesting 'Logga in'-page", () => {
       .click({ multiple: true })
       .get("#outlined-multiline-static")
       .type(`${newText}`);
-  });
 
-  // it("test username & password not being correct", () => {
-  //   cy.visit("/login");
-  //   const username = Cypress.env("test_login");
-  //   const password = Cypress.env("test_passw");
-  //   cy.request("GET", "/login", {
-  //     username: `${username}`,
-  //     userpassword: `${password}`,
-  //   })
-  //     .get(".MuiButton-root")
-  //     .click()
-  //     .get(".tss-194dj1g-emptyWarning > .MuiTypography-root")
-  //     .should("be.visible");
+    //we have not a btn to remove reviews-- there for test ends here.
+    cy.get('[href="/mypage"] > .MuiButtonBase-root').click();
+    cy.get(".MuiButton-root").click();
+  });
 });
 
 export {};
-
-//ÄR inte inloggad- måste klicka på inloggningen och se så det går igenom.
